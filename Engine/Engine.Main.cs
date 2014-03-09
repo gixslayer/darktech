b6a5abbc4e7c64bb531e73f544a10660b784528e
gixslayer <ciske_harsema_2@hotmail.com>
2014-03-09 18:07:01 +0100

    Very rough engine initialization

--- Engine/Engine.Main.cs
+++ Engine/Engine.Main.cs
@@ -1,4 +1,6 @@
-﻿using DarkTech.Engine.Resources;
+﻿using System;
+
+using DarkTech.Engine.Resources;
 using DarkTech.Engine.Scripting;
 
 namespace DarkTech.Engine
@@ -9,6 +11,31 @@ namespace DarkTech.Engine
         public static FileSystem FileSystem { get; private set; }
         public static ResourceManager ResourceManager { get; private set; }
         public static ScriptingInterface ScriptingInterface { get; private set; }
-        public static bool CheatsEnabled { get; private set; }
+        public static EventDispatcher EventDispatcher { get; private set; }
+
+        public static bool CheatsEnabled { get { return ScriptingInterface.GetCvar<CvarBool>("sv_cheats"); } }
+
+        public static bool Initialize()
+        {
+            AttachPrintStream(Console.Out);
+
+            Scene = new SceneGraph();
+            ResourceManager = new ResourceManager();
+            ScriptingInterface = new ScriptingInterface();
+            EventDispatcher = new EventDispatcher();
+
+            try
+            {
+                FileSystem = Platform.CreateFileSystem();
+            }
+            catch (PlatformNotSupportedException e)
+            {
+                Error(e.Message);
+
+                return false;
+            }
+
+            return true;
+        }
     }
 }
---
+++ Engine/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using DarkTech.Engine.Resources;
+
+namespace DarkTech.Engine
+{
+    internal static class Platform
+    {
+        public static FileSystem CreateFileSystem()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    return new FileSystemWindows();
+                default:
+                    throw new PlatformNotSupportedException("Could not create FileSystem implementation for current platform");
+            }
+        }
+    }
+}
