9bb1b8ae3f03c61fbd55885f3a337e00c00b01c9
gixslayer <ciske_harsema_2@hotmail.com>
2014-04-18 14:16:27 +0200

    PC sync

---
+++ Engine/Network/UDPSocket.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace DarkTech.Engine.Network
+{
+    public abstract class UDPSocket
+    {
+        protected readonly EndPoint remoteEP;
+
+        public UDPSocket(EndPoint remoteEP)
+        {
+            this.remoteEP = remoteEP;
+        }
+
+        public abstract void Send(byte[] buffer, int offset, int count);
+        public abstract int Receive(byte[] buffer, ref EndPoint remoteEP);
+    }
+}
---
+++ Engine/Network/UDPSocketWindows.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace DarkTech.Engine.Network
+{
+    public class UDPSocketWindows : UDPSocket
+    {
+        private Socket socket;
+
+        public UDPSocketWindows(EndPoint remoteEP) : base(remoteEP)
+        {
+            this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            this.socket.Blocking = false;
+        }
+
+        public override void Send(byte[] buffer, int offset, int size)
+        {
+            this.socket.SendTo(buffer, offset, size, SocketFlags.None, remoteEP);
+        }
+
+        public override int Receive(byte[] buffer, ref EndPoint remoteEP)
+        {
+            return this.socket.ReceiveFrom(buffer, ref remoteEP);
+        }
+    }
+}
