5c77052fe89b331c6eaa2355cd1cc3f1cfdccd4e
gixslayer <ciske_harsema_2@hotmail.com>
2014-04-23 16:22:00 +0200

    Fixed Common/Debug being ignored

---
+++ Common/Debug/Assert.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DarkTech.Common.Debug
+{
+    public static class Assert
+    {
+        [Conditional("DEBUG")]
+        public static void IsNull(object obj, string message)
+        {
+            if (obj != null)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+        
+        [Conditional("DEBUG")]
+        public static void IsNotNull(object obj, string message)
+        {
+            if (obj == null)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void True(bool condition, string message)
+        {
+            if (!condition)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void False(bool condition, string message)
+        {
+            if (condition)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void ReferenceEquals(object a, object b, string message)
+        {
+            if (!object.ReferenceEquals(a, b))
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void Equals<T>(T a, T b, string message) where T : IComparable
+        {
+            if (a.CompareTo(b) != 0)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void Greater<T>(T a, T b, string message) where T : IComparable
+        {
+            if (a.CompareTo(b) <= 0)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void GreaterOrEqual<T>(T a, T b, string message) where T : IComparable
+        {
+            if (a.CompareTo(b) < 0)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void Less<T>(T a, T b, string message) where T : IComparable
+        {
+            if (a.CompareTo(b) >= 0)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+        
+        [Conditional("DEBUG")]
+        public static void LessOrEqual<T>(T a, T b, string message) where T : IComparable
+        {
+            if (a.CompareTo(b) > 0)
+            {
+                Trace.TraceError(message);
+
+                Debugger.Break();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        public static void Fail(string message)
+        {
+            Trace.TraceError(message);
+
+            Debugger.Break();
+        }
+    }
+}
---
+++ Common/Debug/Profiler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DarkTech.Common.Debug
+{
+    public static class Profiler
+    {
+        private static readonly Dictionary<string, DateTime> sections = new Dictionary<string, DateTime>();
+        private static readonly Dictionary<string, TimeSpan> results = new Dictionary<string, TimeSpan>();
+        private static TimeSpan total = TimeSpan.Zero;
+
+        public static TimeSpan Total { get { return total; } }
+        public static Dictionary<string, TimeSpan>.KeyCollection Sections { get { return results.Keys; } }
+
+        public static void Begin(string section)
+        {
+            if (sections.ContainsKey(section))
+                throw new InvalidOperationException("Section " + section + " has not ended");
+
+            if (results.ContainsKey(section))
+            {
+                results.Remove(section);
+            }
+
+            sections.Add(section, DateTime.Now);
+        }
+
+        public static TimeSpan End(string section)
+        {
+            if (!sections.ContainsKey(section))
+                throw new InvalidOperationException("Section " + section + " has not been started");
+
+            DateTime end = DateTime.Now;
+            DateTime start = sections[section];
+
+            TimeSpan difference = end.Subtract(start);
+
+            results.Add(section, difference);
+            total.Add(difference);
+
+            return difference;
+        }
+
+        public static void Reset()
+        {
+            sections.Clear();
+            results.Clear();
+
+            total = TimeSpan.Zero;
+        }
+
+        public static TimeSpan Get(string section)
+        {
+            if (!Has(section))
+                throw new ArgumentException("Profiler does not contain information for section " + section);
+
+            return results[section];
+        }
+
+        public static float GetFraction(string section)
+        {
+            if (!Has(section))
+                throw new ArgumentException("Profiler does not contain information for section " + section);
+
+            // If the total time is under 1 tick the 'time spend' is distributed evenly across all sections.
+            if (total.Ticks == 0)
+            {
+                return 1f / (float)results.Count;
+            }
+
+            TimeSpan sectionTime = results[section];
+
+            return (float)((double)sectionTime.Ticks / (double)total.Ticks);
+        }
+
+        public static bool Has(string section)
+        {
+            return results.ContainsKey(section);
+        }
+    }
+}
