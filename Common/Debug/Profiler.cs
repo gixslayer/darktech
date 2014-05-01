72df66eca526176af40f8564382537c732a28caf
gixslayer <ciske_harsema_2@hotmail.com>
2014-05-01 17:39:24 +0200

    Added documentation and some minor changes

--- Common/Debug/Profiler.cs
+++ Common/Debug/Profiler.cs
@@ -3,79 +3,130 @@ using System.Collections.Generic;
 
 namespace DarkTech.Common.Debug
 {
+    /// <summary>
+    /// Provides a basic profiling solution to measure executing time for code sections.
+    /// </summary>
     public static class Profiler
     {
-        private static readonly Dictionary<string, DateTime> sections = new Dictionary<string, DateTime>();
-        private static readonly Dictionary<string, TimeSpan> results = new Dictionary<string, TimeSpan>();
+        private static readonly Dictionary<string, DateTime> ACTIVE_SECTIONS = new Dictionary<string, DateTime>();
+        private static readonly Dictionary<string, TimeSpan> COMPLETED_SECTIONS = new Dictionary<string, TimeSpan>();
+        private static readonly TimeSpan TIMESPAN_ONE_TICK = new TimeSpan(1);
+
         private static TimeSpan total = TimeSpan.Zero;
 
+        /// <summary>
+        /// The total time spend profiling.
+        /// </summary>
         public static TimeSpan Total { get { return total; } }
-        public static Dictionary<string, TimeSpan>.KeyCollection Sections { get { return results.Keys; } }
+        /// <summary>
+        /// The collection of sections that have information available.
+        /// </summary>
+        public static Dictionary<string, TimeSpan>.KeyCollection Sections { get { return COMPLETED_SECTIONS.Keys; } }
 
+        /// <summary>
+        /// Begins profiling a new section.
+        /// </summary>
+        /// <param name="section">The name of the section.</param>
+        /// <exception cref="InvalidOperationException">Thrown when the specified <paramref name="section"/> has not ended.</exception>
         public static void Begin(string section)
         {
-            if (sections.ContainsKey(section))
+            if (ACTIVE_SECTIONS.ContainsKey(section))
                 throw new InvalidOperationException("Section " + section + " has not ended");
 
-            if (results.ContainsKey(section))
-            {
-                results.Remove(section);
-            }
-
-            sections.Add(section, DateTime.Now);
+            ACTIVE_SECTIONS.Add(section, DateTime.Now);
         }
 
+        /// <summary>
+        /// Ends profiling a section and returns the total time spend for that section.
+        /// </summary>
+        /// <param name="section">The name of the section.</param>
+        /// <returns>Returns a <c>TimeSpan</c> with the total time spend in the <paramref name="section"/>.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the specified <paramref name="section"/> has not been started.</exception>
+        /// <remarks>
+        /// When previous information is found for the <paramref name="section"/> the new information is added to that information.
+        /// The time spend is guaranteed to be at least one tick.
+        /// </remarks>
         public static TimeSpan End(string section)
         {
-            if (!sections.ContainsKey(section))
+            if (!ACTIVE_SECTIONS.ContainsKey(section))
                 throw new InvalidOperationException("Section " + section + " has not been started");
 
+            // Calculate the time spend on the section.
             DateTime end = DateTime.Now;
-            DateTime start = sections[section];
+            DateTime start = ACTIVE_SECTIONS[section];
+            TimeSpan duration = end.Subtract(start);
 
-            TimeSpan difference = end.Subtract(start);
+            // Ensure the duration is at least one tick.
+            if (duration.Ticks == 0)
+            {
+                duration = TIMESPAN_ONE_TICK;
+            }
 
-            results.Add(section, difference);
-            total.Add(difference);
+            // Add the time to the existing entry or create a new entry if it does not exist.
+            if (COMPLETED_SECTIONS.ContainsKey(section))
+            {
+                COMPLETED_SECTIONS[section] += duration;
+            }
+            else
+            {
+                COMPLETED_SECTIONS.Add(section, duration);
+            }
+            
+            // Increase the total time.
+            total.Add(duration);
 
-            return difference;
+            return COMPLETED_SECTIONS[section];
         }
 
+        /// <summary>
+        /// Resets the <see cref="Profiler"/> by clearing all previous information and setting the total time to zero.
+        /// </summary>
         public static void Reset()
         {
-            sections.Clear();
-            results.Clear();
+            ACTIVE_SECTIONS.Clear();
+            COMPLETED_SECTIONS.Clear();
 
             total = TimeSpan.Zero;
         }
 
+        /// <summary>
+        /// Returns a <c>TimeSpan</c> with the total time spend in the <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">The name of the section.</param>
+        /// <returns>Returns a <c>TimeSpan</c> with the total time spend in the <paramref name="section"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when the specified <paramref name="section"/> has no information available.</exception>
         public static TimeSpan Get(string section)
         {
             if (!Has(section))
                 throw new ArgumentException("Profiler does not contain information for section " + section);
 
-            return results[section];
+            return COMPLETED_SECTIONS[section];
         }
 
-        public static float GetFraction(string section)
+        /// <summary>
+        /// Returns a <c>double</c> in the range of [0.0, 1.0] with the fraction of the total time spend in the <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">The name of the section.</param>
+        /// <returns>Returns a <c>double</c> in the range of [0.0, 1.0] with the fraction of the total time spend in the <paramref name="section"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when the specified <paramref name="section"/> has no information available.</exception>
+        public static double GetFraction(string section)
         {
             if (!Has(section))
                 throw new ArgumentException("Profiler does not contain information for section " + section);
 
-            // If the total time is under 1 tick the 'time spend' is distributed evenly across all sections.
-            if (total.Ticks == 0)
-            {
-                return 1f / (float)results.Count;
-            }
+            TimeSpan sectionTime = COMPLETED_SECTIONS[section];
 
-            TimeSpan sectionTime = results[section];
-
-            return (float)((double)sectionTime.Ticks / (double)total.Ticks);
+            return (double)sectionTime.Ticks / (double)total.Ticks;
         }
 
+        /// <summary>
+        /// Checks if the specified <paramref name="section"/> has information available.
+        /// </summary>
+        /// <param name="section">The name of the section.</param>
+        /// <returns>Returns <c>true</c> if the specified <paramref name="section"/> has information available, otherwise <c>false</c> is returned.</returns>
         public static bool Has(string section)
         {
-            return results.ContainsKey(section);
+            return COMPLETED_SECTIONS.ContainsKey(section);
         }
     }
 }
